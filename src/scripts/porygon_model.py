244a2b71fc85fc39e0b52e4a5305da01e2e99e11
Anthony Krivonos <abkrivonos@gmail.com>
2020-06-25 20:31:28 -0400

    MCST kinda lookin nice (#9)
    * oh yea u gotta pip install pptree
    
    * MCST kinda lookin nice
    
    Co-authored-by: Kenneth Chuen <kc3334@columbia.edu>
    Co-authored-by: Anthony Krivonos <akrivonos@lyft.com>

--- src/scripts/porygon_model.py
+++ src/scripts/porygon_model.py
@@ -10,7 +10,7 @@ from src.ai import PorygonModel
 
 
 party1 = get_party('charizard', 'charizard', 'charizard')
-party2 = get_party('bulbasaur', 'bulbasaur', 'bulbasaur')
+party2 = get_party('venusaur', 'venusaur', 'venusaur')
 
 player1 = Player("Player 1", party1, Bag())
 player2 = Player("Player 2", party2, Bag(), PorygonModel)
